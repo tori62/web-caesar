788cbd4b677a2191290cd2404939c38889d3336a
Tori Gonzalez <tori@nexmatix.com>
2018-03-23 08:04:13 -0500

    added HTMLform outline

--- main.py
+++ main.py
@@ -3,6 +3,33 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    broder-radius: 10px;
+                }
+
+                textarea{
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+
+
+        </body>
+    </html> """
+
 @app.route("/")
 def index():
 
